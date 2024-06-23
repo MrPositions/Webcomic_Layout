877c614961bffbdd3f20935c027c3b32204d1145
MrPositions <dotasmurf4444@gmail.com>
2024-06-23 09:11:59 +0500

    Webcomic Skeleton

--- apilab.js
+++ apilab.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
             const userContainer = document.getElementById('user-container');
 
-            fetch('https://github.com/Ovi/DummyJSON/blob/master/database/users.json')
+            fetch('http://dummyjson.com/users')
                 .then(response => response.json())
                 .then(data => {
                     console.log(data); // Log the data to see its structure
